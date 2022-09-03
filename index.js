9cbe590ae53d4f55efba17aa54886170ab40f834
Dipti saha <sahadipti322@gmail.com>
2022-09-03 08:31:30 -0400

    spiner add

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const newsData = async()=>{
-const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+  const message = document.getElementById('err');
+  message.innerHTML = "";
+try{
+  const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
 const data = await res.json();
  return data.data.news_category;
+}catch(error){
+  message.innerHTML = "Input is " + error;
+}
 }
 //newsData();
 const itemData = async(category)=>{
+  const message = document.getElementById('err');
+  message.innerHTML = "";
+try{
     const url = `https://openapi.programming-hero.com/api/news/category/${category}`;
     const res = await fetch(url);
     const data = await res.json();
      return data.data;
+}catch(error){
+  message.innerHTML = "Input is " + error;
+}
     }
 //itemFound();
 const displayItem = async(category)=>{
     const itemInfo = await itemData(category);
-    console.log(itemInfo);
+    console.log(itemInfo); 
     const itemField = document.getElementById('display-item');
     itemField.textContent = "";
     const itemCountField = document.getElementById("item-count");
     itemCountField.innerText ='';
-    itemCountField.innerText = itemInfo.length;
+    if(itemInfo.length>0){
+      itemCountField.innerText = itemInfo.length;
+    }else{
+      itemCountField.innerText = "No";
+    }
     console.log(itemInfo.length);
     itemInfo.forEach((item) =>{
        // console.log(item,count);
@@ -38,12 +54,12 @@ const displayItem = async(category)=>{
                   <div class="d-flex">
                   <img src="${author.img}" class=" author-img" alt="...">
                   <div>
-                  <h3 class="fs-6">${author.name}<h3>
+                  <h3 class="fs-6">${author.name?author.name:'No author name'}<h3>
                   <p class="fs-6">${author.published_date}</p>
                   </div> 
                   </div>
                   <div>
-                  <p class="fs-6"> <i class="fa-regular fa-eye"></i>  ${item.total_view}</p>
+                  <p class="fs-6"> <i class="fa-regular fa-eye"></i>  ${item.total_view?item.total_view:'No view'}</p>
                   </div>
                   <div>
                   <button onclick="detailData('${item._id}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
@@ -58,10 +74,11 @@ const displayItem = async(category)=>{
         
         `;
         itemField.appendChild(div);
+        toggleSpinner(false);
 
     })
 } 
-displayItem('03');
+//displayItem('03');
 
 const displayCatagory= async()=>{
 const categoryData = await newsData();
@@ -72,7 +89,7 @@ categoryData.forEach(item => {
     //console.log(item);
     const div = document.createElement('div');
     div.innerHTML = `
-    <h3 onclick="displayItem('${item.category_id?item.category_id:'Category not found'}')"  class="fs-6 text-decoration-none">${item.category_name}</h3>
+    <h3 onclick="displayItem('${item.category_id}');toggleSpinner(${true})"  class="fs-6 text-decoration-none">${item.category_name}</h3>
     `;
     itemContainer.appendChild(div);
     
@@ -98,8 +115,25 @@ const detailData= async(id)=>{
         <h6>${items.title}</h6>
         <img src="${items.image_url}" class="img-fluid rounded-start" alt="...">
         <p>${items.details}</p>
+        <div class="d-flex">
+                  <img src="${items.author.img}" class=" author-img" alt="...">
+                  <div>
+                  <h3 class="fs-6">${items.author.name?items.author.name:'No author name'}<h3>
+                  <p class="fs-6">${items.author.published_date?items.author.published_date:"No date publish"}</p>
+                  </div> 
+                  </div>
+
         `
         modal.appendChild(div);
       // })
     }
+    const toggleSpinner = isLoading => {
+      const spinerSection = document.getElementById('spinner');
+      if(isLoading){
+        spinerSection.classList.remove('d-none')
+      }
+      else{
+        spinerSection.classList.add('d-none');
+      }
+  }
     
